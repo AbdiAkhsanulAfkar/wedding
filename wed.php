3465abe56d28d61c5828a34a2b2752bd3d3bd1ba
faamzero <singpentingyakin17@gmail.com>
2021-08-16 16:48:59 +0700

    yahahahayu

---
+++ wed.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Bootstrap 4 Carousel</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://kit.fontawesome.com/3bb7b7789b.js" crossorigin="anonymous"></script>
+
+<style>
+.carousel{
+	position: fixed;
+	top: 50%;
+	left: 50%;
+	margin-top: -150px;
+	margin-left: -250px;
+
+}
+.carousel-item{
+    text-align: center;
+     /* Prevent carousel from being distorted if for some reason image doesn't load */
+}
+.card{
+  height:300px;
+  width:500px;
+}
+</style>
+</head>
+<body style="background-color:peachpuff;">
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<a href="#myCarousel" data-slide="prev"> <i class="fas fa-chevron-circle-left fa-2x" style="padding-left: 450px; color:black"></i></a>
+<a href="#myCarousel" data-slide="next"> <i class="fas fa-chevron-circle-right fa-2x" style="padding-left: 565px; color:black"></i></a>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+          <div class="card"">
+          <img class="card-img-top" src="b.jpg" alt="First slide">
+          <div class="card-body">
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="card"">
+         <img class="card-img-top" src="b.jpg" alt="Second slide">
+        <div class="card-body">
+    </div>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <div class="card">
+      <img class="card-img-top" src="b.jpg" alt="Third slide">
+      <div class="card-body">
+    </div>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <div class="card"">
+         <img class="card-img-top" src="b.jpg" alt="Fourth slide">
+        <div class="card-body">
+    </div>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <div class="card"">
+         <img class="card-img-top" src="b.jpg" alt="Sixth slide">
+        <div class="card-body">
+    </div>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <div class="card"">
+         <img class="card-img-top" src="b.jpg" alt="Seventh slide">
+        <div class="card-body">
+    </div>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <div class="card"">
+         <img class="card-img-top" src="b.jpg" alt="Eighth">
+        <div class="card-body">
+    </div>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <div class="card"">
+         <img class="card-img-top" src="b.jpg" alt="Nineth slide">
+        <div class="card-body">
+    </div>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <div class="card"">
+         <img class="card-img-top" src="b.jpg" alt="Tenth slide">
+        <div class="card-body">
+    </div>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <div class="card"">
+         <img class="card-img-top" src="b.jpg" alt="Eleventh slide">
+        <div class="card-body">
+    </div>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <div class="card"">
+         <img class="card-img-top" src="b.jpg" alt="Twelfth slide">
+        <div class="card-body">
+    </div>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <div class="card"">
+         <img class="card-img-top" src="b.jpg" alt="Thirteentg=h slide">
+        <div class="card-body">
+    </div>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <div class="card"">
+         <img class="card-img-top" src="b.jpg" alt="Fourteenth slide">
+        <div class="card-body">
+    </div>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <div class="card"">
+         <img class="card-img-top" src="b.jpg" alt="Fifteenth slide">
+        <div class="card-body">
+    </div>
+    </div>
+    </div>
+</div>
+</body>
+</html>                            
